4c5ad096ec0002dbf495a5285114376996aee695
Alexandros Fourtounis <theadventurer2003@gmail.com>
2024-05-26 02:43:13 +0300

    parsing for register and unregister of user

--- src/libs/utils.c
+++ src/libs/utils.c
@@ -1,1 +1,37 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+int parse_file(const char *filename){
+    if(!filename){
+        fprintf(stderr, "filename is null\n");
+    }
+    FILE *file = fopen(filename, "r");
+    if(file == NULL){
+        fprintf(stderr, "Error opening file\n");
+        return -1;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char action[2];
+    int id;
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        sscanf(line, "- %s %d", action, &id);
+        if (strcmp(action, "R") == 0)
+        {
+            printf("User with ID %d registered\n", id);
+        }
+        else if(strcmp(action,"U") == 0){
+            printf("User with ID %d unregistered\n", id);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
