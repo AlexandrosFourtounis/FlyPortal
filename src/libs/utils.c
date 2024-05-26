000d4f0bdcd2aeaec56327953dc44cb070565842
SteliosAinalis <steliosain@hotmail.com>
2024-05-26 20:20:09 +0300

    added insert_flight_schedule function and a flight print function

--- src/libs/utils.c
+++ src/libs/utils.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 user *users = NULL;
+flight_info *flights =NULL;
 
 
 
@@ -13,7 +14,7 @@ void registerUser(int uid, char *name, char *email, char *password, char *phone,
     user *current = users;
     while (current != NULL) {
         if (uid == current->uid) {
-            printf("User with UID %d already exists\n", uid);
+            printf("User with uid %d already exists\n", uid);
             return;
         }
         current = current->next;
@@ -77,13 +78,13 @@ void deleteUser(int uid, char *name){
             free(current->zip);
             free(current->country);
             free(current);
-            printf("User %c with uid %i erased\n",*name, uid);
+            printf("User %s with uid %i erased\n", name, uid);
             return;
         }
         prev = current;
         current = current->next;
     }
-    printf("User with uid %i does not exist\n", uid);
+    printf("User %s with uid %i does not exist\n", name, uid);
 }
 
 int print_users(){
@@ -91,6 +92,59 @@ int print_users(){
     user *current = users;
     while(current != NULL){
         printf("Name: %s\n", current->name);
+        current = current->next;
+    }
+}
+
+flight_node *flights_head = NULL;
+
+void insert_flight_schedule(airlines_t airline, char *flight_number, char *departure, char *arrival, char *departure_time, char *arrival_time, char *price) {
+   
+    flight_info *newFlight = (flight_info *)malloc(sizeof(flight_info));
+    if (newFlight == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newFlight->airline = airline;
+    newFlight->flight_number = strdup(flight_number);
+    newFlight->departure = strdup(departure);
+    newFlight->arrival = strdup(arrival);
+    newFlight->departure_time = strdup(departure_time);
+    newFlight->arrival_time = strdup(arrival_time);
+    newFlight->price = strdup(price);
+    
+    flight_node *newNode = (flight_node *)malloc(sizeof(flight_node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->flight = newFlight;
+    newNode->next = NULL;
+    
+    if (flights_head == NULL) {
+        flights_head = newNode;
+    } else {
+        flight_node *temp = flights_head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void print_flights() {
+    printf("Printing flights\n");
+    flight_node *current = flights_head;
+    while (current != NULL) {
+        printf("Airline: %d\n", current->flight->airline);
+        printf("Flight Number: %s\n", current->flight->flight_number);
+        printf("Departure: %s\n", current->flight->departure);
+        printf("Arrival: %s\n", current->flight->arrival);
+        printf("Departure Time: %s\n", current->flight->departure_time);
+        printf("Arrival Time: %s\n", current->flight->arrival_time);
+        printf("Price: %s\n", current->flight->price);
         current = current->next;
     }
 }
--- src/main.c
+++ src/main.c
@@ -9,12 +9,23 @@ int main(int argc, char **argv){
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    registerUser(1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+    registerUser(2,"Stelios",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+    deleteUser(3,"Alex");
+    registerUser(3,"Alex",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+    deleteUser(3,"Alex");
+    insert_flight_schedule(1, "12abc", "katerini", "dubai", "12", "6", "500$\n");
+    print_flights();
+    
 
+
+    
     const char *filename = argv[1];
     if(parse_file(filename) == -1){
         fprintf(stderr, "Error parsing file\n");
         return -1;
     }
+    
     print_users();
 
     return 0;
