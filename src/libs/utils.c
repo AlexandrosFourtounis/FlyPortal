c74aeab11961767e17888f989aab7bac70c37925
Alexandros Fourtounis <theadventurer2003@gmail.com>
2024-05-26 03:29:24 +0300

    fixed parse_file

--- src/libs/utils.c
+++ src/libs/utils.c
@@ -72,20 +72,13 @@ int parse_file(const char *filename)
 
     while ((read = getline(&line, &len, file)) != -1)
     {
-        if (line[1] == 'R')
+        if (line[0] == '-')
         {
             int id;
-            char name[50], email[50], password[50], dob[50], city[50], state[50], country[50];
-            int zip;
-            sscanf(line, "%*s %d %s %s %s %s %s %s %d %s", &id, name, email, password, dob, city, state, &zip, country);
-            printf("Action: R, ID: %d, Name: %s, Email: %s, Password: %s, DOB: %s, City: %s, State: %s, ZIP: %d, Country: %s\n", id, name, email, password, dob, city, state, zip, country);
-        }
-        else if (line[1] == 'A')
-        {
-            char mid[50], company[50], departure_time[50], arrival_time[50], arrival_location[50], departure_loc[50];
-            int cost;
-            sscanf(line, "%*s %s %s %d %s %s %s %s", mid, company, &cost, departure_time, arrival_time, arrival_location, departure_loc);
-            printf("Action: A, MID: %s, Company: %s, Cost: %d, Departure Time: %s, Arrival Time: %s, Arrival Location: %s, Departure Location: %s\n", mid, company, cost, departure_time, arrival_time, arrival_location, departure_loc);
+            char action[2], name[50], email[50], password[50], telephone[50], address[50], city[50], state[50], zip[50], country[50];
+            sscanf(line, "%*s %s %d %s %s %s %s %s %s %s %s %s", action, &id, name, email, password, telephone, address, city, state, zip, country);
+            // printf("Action: %s, ID: %d, Name: %s, Email: %s, Password: %s, Telephone: %s, Address: %s, City: %s, State: %s, ZIP: %s, Country: %s\n", action, id, name, email, password, telephone, address, city, state, zip, country);
+            registerUser(id, name, email, password, telephone, address, city, state, zip, country);
         }
         else
             printf("what?");
